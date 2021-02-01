0758495be1d9c7827b8cb1a793cd60f7582b3c9c
Tairy <tairyguo@gmail.com>
2021-02-01 21:51:33 +0800

    add resource manager

--- src/main/java/com/mechempire/client/manager/ResourceManager.java
+++ src/main/java/com/mechempire/client/manager/ResourceManager.java
@@ -1,6 +1,9 @@
 package com.mechempire.client.manager;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
 
 /**
  * package: com.mechempire.client.manager
@@ -8,6 +11,13 @@ import org.springframework.stereotype.Component;
  * @author <tairy> tairyguo@gmail.com
  * @date 2021/1/31 下午8:51
  */
+@Slf4j
 @Component
 public class ResourceManager {
+
+    private HashMap<String, String> fonts = new HashMap<>(16);
+
+    private HashMap<String, String> images = new HashMap<>(16);
+
+    private HashMap<String, String> media = new HashMap<>(16);
 }
